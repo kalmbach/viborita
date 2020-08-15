50b8dbda2192be72984ae9cbe916df0d2f50365e
Jorge <kalmbach@gmail.com>
2020-08-15 17:29:38 -0300

    Add score and initial length

--- js/viborita.js
+++ js/viborita.js
@@ -4,7 +4,7 @@ let ctx;
 
 const defaultTailSize = 3;
 let tailSize = defaultTailSize;
-const snakeTrail = [];
+const snakeTrail = [{ x: 8, y: 10 }, { x: 9, y: 10 }, { x: 10, y: 10 }];
 let snakeX = 10;
 let snakeY = 10;
 
@@ -15,6 +15,86 @@ let nextY = 0;
 
 let appleX = 15;
 let appleY = 15;
+
+const tps = 6; // Time Per Second
+let drawInterval;
+let score = 0;
+
+function paintBoard() {
+  // reset background
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function paintApple() {
+  // paint apple
+  ctx.fillStyle = 'red';
+  ctx.fillRect(appleX * tileSize, appleY * tileSize, tileSize, tileSize);
+}
+
+function paintSnake() {
+  // paint snake
+  ctx.fillStyle = 'purple';
+  ctx.fillRect(
+    snakeTrail[snakeTrail.length - 1].x * tileSize,
+    snakeTrail[snakeTrail.length - 1].y * tileSize,
+    tileSize,
+    tileSize,
+  );
+
+  ctx.fillStyle = 'green';
+  for (let i = 0; i < snakeTrail.length - 1; i += 1) {
+    ctx.fillRect(
+      snakeTrail[i].x * tileSize,
+      snakeTrail[i].y * tileSize,
+      tileSize,
+      tileSize,
+    );
+
+    if (snakeTrail[i].x === snakeX && snakeTrail[i].y === snakeY) {
+      tailSize = defaultTailSize;
+      score = 0;
+      document.getElementById('score').innerText = score;
+    }
+  }
+}
+
+function draw() {
+  snakeX += nextX;
+  snakeY += nextY;
+
+  if (snakeX < 0) {
+    snakeX = gridSize - 1;
+  }
+  if (snakeX > gridSize - 1) {
+    snakeX = 0;
+  }
+  if (snakeY < 0) {
+    snakeY = gridSize - 1;
+  }
+  if (snakeY > gridSize - 1) {
+    snakeY = 0;
+  }
+
+  paintBoard();
+  paintApple();
+  paintSnake();
+
+  if (snakeX === appleX && snakeY === appleY) {
+    tailSize += 1;
+    score += 1;
+    appleX = Math.floor(Math.random() * gridSize);
+    appleY = Math.floor(Math.random() * gridSize);
+
+    document.getElementById('score').innerText = score;
+  }
+
+  // set snake trail
+  snakeTrail.push({ x: snakeX, y: snakeY });
+  while (snakeTrail.length > tailSize) {
+    snakeTrail.shift();
+  }
+}
 
 function keyDownEvent(e) {
   switch (e.keyCode) {
@@ -38,58 +118,9 @@ function keyDownEvent(e) {
       nextX = 0;
       nextY = 0;
   }
-}
 
-function draw() {
-  snakeX += nextX;
-  snakeY += nextY;
-
-  if (snakeX < 0) {
-    snakeX = gridSize - 1;
-  }
-  if (snakeX > gridSize - 1) {
-    snakeX = 0;
-  }
-  if (snakeY < 0) {
-    snakeY = gridSize - 1;
-  }
-  if (snakeY > gridSize - 1) {
-    snakeY = 0;
-  }
-
-  // reset background
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  // paint apple
-  ctx.fillStyle = 'red';
-  ctx.fillRect(appleX * tileSize, appleY * tileSize, tileSize, tileSize);
-
-  if (snakeX === appleX && snakeY === appleY) {
-    tailSize += 1;
-    appleX = Math.floor(Math.random() * gridSize);
-    appleY = Math.floor(Math.random() * gridSize);
-  }
-
-  // paint snake
-  ctx.fillStyle = 'green';
-  for (let i = 0; i < snakeTrail.length; i += 1) {
-    ctx.fillRect(
-      snakeTrail[i].x * tileSize,
-      snakeTrail[i].y * tileSize,
-      tileSize,
-      tileSize,
-    );
-
-    if (snakeTrail[i].x === snakeX && snakeTrail[i].y === snakeY) {
-      tailSize = defaultTailSize;
-    }
-  }
-
-  // set snake trail
-  snakeTrail.push({ x: snakeX, y: snakeY });
-  while (snakeTrail.length > tailSize) {
-    snakeTrail.shift();
+  if (!drawInterval) {
+    drawInterval = setInterval(draw, 1000 / tps);
   }
 }
 
@@ -97,8 +128,9 @@ window.onload = function () {
   canvas = document.getElementById('canvas');
   ctx = canvas.getContext('2d');
 
-  document.addEventListener('keydown', keyDownEvent);
+  paintBoard();
+  paintApple();
+  paintSnake();
 
-  const tps = 6; // Time Per Second
-  setInterval(draw, 1000 / tps);
+  document.addEventListener('keydown', keyDownEvent);
 };
